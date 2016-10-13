3d117106b0254b3023df5cbd281b509661b3c8a9
Teresa Chaves <up201306842@fe.up.pt>
2016-10-13 09:44:49 +0100

    errors begin.

--- TP1/WebCGF.XMLReaderExample/reader/MySceneGraph.js
+++ TP1/WebCGF.XMLReaderExample/reader/MySceneGraph.js
@@ -114,18 +114,34 @@ MySceneGraph.prototype.onXMLReady=function()
 MySceneGraph.prototype.parseScene = function(rootElement) {
 
 	var elems =  rootElement.getElementsByTagName('scene');
+
 	if (elems == null) { // errro não existe um scene - reporta e termina
-		return "scene element is missing.";
+		return "Scene element is missing.";
 	}
 
 	if (elems.length != 1) { // erro tem mais do que um scene - reporta e termina
-		return "either zero or more than one 'scene' element found.";
+		return "Either zero or more than one 'scene' element found.";
 	}
 
-	// various examples of different types of access
 	var scene = elems[0];
-	this.root = this.reader.getString(scene, 'root');
-	this.axis_length = this.reader.getFloat(scene, 'axis_length');
+
+	this.root = this.reader.getString(scene, 'root', true);
+
+	// errors
+	if(this.root == null) {
+		return "Attribute root not found.";
+	}
+
+	this.axis_length = this.reader.getFloat(scene, 'axis_length', true);
+
+	// errors
+	if(this.axis_length == null) {
+		return "Attribute axis_length not found.";
+	}
+
+	if(isNaN(this.axis_length)) {
+		return "Attribute axis_length must be a float number.";
+	}
 
 	console.log("Scene read from file: {root=" + this.root + ", axis_length=" + this.axis_length + "}");
 
@@ -972,49 +988,6 @@ MySceneGraph.prototype.parseComponents = function(rootElement) {
 }
 
 /*
- * Example of method that parses elements of one block and stores information in a specific data structure
- */
-MySceneGraph.prototype.parseGlobalsExample= function(rootElement) {
-
-	var elems =  rootElement.getElementsByTagName('globals');
-	if (elems == null) { // erro não existe um globals - reporta e termina
-		return "globals element is missing.";
-	}
-
-	if (elems.length != 1) { // erro tem mais do que um globals - reporta e termina
-		return "either zero or more than one 'globals' element found.";
-	}
-
-	// various examples of different types of access
-	var globals = elems[0];
-	this.background = this.reader.getRGBA(globals, 'background');
-	this.drawmode = this.reader.getItem(globals, 'drawmode', ["fill","line","point"]);
-	this.cullface = this.reader.getItem(globals, 'cullface', ["back","front","none", "frontandback"]);
-	this.cullorder = this.reader.getItem(globals, 'cullorder', ["ccw","cw"]);
-
-	console.log("Globals read from file: {background=" + this.background + ", drawmode=" + this.drawmode + ", cullface=" + this.cullface + ", cullorder=" + this.cullorder + "}");
-
-	var tempList=rootElement.getElementsByTagName('list');
-
-	if (tempList == null  || tempList.length==0) {
-		return "list element is missing.";
-	}
-
-	this.list=[];
-	// iterate over every element
-	var nnodes=tempList[0].children.length;
-	for (var i=0; i< nnodes; i++)
-	{
-		var e=tempList[0].children[i];
-
-		// process each element and store its information
-		this.list[e.id]=e.attributes.getNamedItem("coords").value;
-		console.log("Read list item id "+ e.id+" with value "+this.list[e.id]);
-	};
-
-};
-
-/*
  * Callback to be executed on any read error
  */
 
@@ -1022,3 +995,7 @@ MySceneGraph.prototype.onXMLError=function (message) {
 	console.error("XML Loading Error: "+message);
 	this.loadedOk=false;
 };
+
+MySceneGraph.prototype.parseStringAttr = function() {
+
+}
